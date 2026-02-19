0d7e4f0490fad94ae5954a438f9af736d7c55a58
Poplizzy <okediyabukunmi@gmail.com>
2026-02-18 18:01:20 -0800

    fix resources

--- routes/resources.js
+++ routes/resources.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getResourcesByLevel, getAllResources } = require('../controllers/resourceController');
+const { getResourcesByLevel, getAllResources } = require('../controllers/resourcecontroller');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
